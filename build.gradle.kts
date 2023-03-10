a3088636b9546dfe601e188a4c1309b99a10a9d4
Christoph Sturm <me@christophsturm.com>
2023-03-10 10:29:07 +0100

    add kotlinter plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
   id("org.jetbrains.dokka") version "1.7.0" apply false
   id("com.gradle.plugin-publish") version "0.11.0" apply false
   id("com.github.gmazzo.buildconfig") version "2.0.2" apply false
+  id("org.jmailen.kotlinter") version "3.13.0" apply false
 }
 
 allprojects {
