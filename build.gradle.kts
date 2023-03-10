d5ac74dfc15d22fb6b1c9c83bdf1b24c89ca8393
Christoph Sturm <me@christophsturm.com>
2023-03-10 09:42:29 +0100

    add versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
+  id("com.github.ben-manes.versions") version "0.46.0"
   kotlin("jvm") version "1.7.0" apply false
   id("org.jetbrains.dokka") version "1.7.0" apply false
   id("com.gradle.plugin-publish") version "0.11.0" apply false
@@ -15,3 +18,23 @@ subprojects {
     mavenCentral()
   }
 }
+
+fun isNonStable(version: String): Boolean {
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+  val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+  val isStable = stableKeyword || regex.matches(version)
+  return isStable.not()
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+  rejectVersionIf {
+    isNonStable(candidate.version) && !isNonStable(currentVersion)
+  }
+  // optional parameters
+  gradleReleaseChannel = "current"
+  checkForGradleUpdate = true
+  outputFormatter = "json"
+  outputDir = "build/dependencyUpdates"
+  reportfileName = "report"
+}
+
