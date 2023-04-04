5324850f6b3f11f384b960ae18e5ec998ac3cc0b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2023-04-04 10:31:20 +0100

    Upgrade plugin-publish to latest 1.1.0
    Action "Deprecate the entire `pluginBundle` block when using Gradle version 7.6 or later,
            mark for removal in version 8.0" from https://plugins.gradle.org/plugin/com.gradle.plugin-publish/1.0.0-rc-3
    
    Gradle publishing has to execute publishPlugins directly, because the new version already applies the Maven Publish Plugin, that creates the task "publish", which publishes to registered repositories only.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   kotlin("jvm") version "1.7.0" apply false
   id("org.jetbrains.dokka") version "1.7.0" apply false
-  id("com.gradle.plugin-publish") version "0.11.0" apply false
+  id("com.gradle.plugin-publish") version "1.1.0" apply false
   id("com.github.gmazzo.buildconfig") version "3.1.0" apply false
 }
 
